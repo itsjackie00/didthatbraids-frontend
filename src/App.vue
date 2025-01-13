d85c9977b3b6e522ba90ef0b7fa8f5dded71fad9
itsjackie00 <jackiedanso33@gmail.com>
2025-01-13 12:45:12 +0100

    added braidstype list component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
     <Navbar />
     <Carosel />
+    <BraidstypeList />
     <router-view></router-view>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import Carosel from './components/Carosel.vue';
+import BraidstypeList from './components/BraidstypeList.vue';
 
 export default {
     components: {
         Navbar,
-        Carosel
+        Carosel,
+        BraidstypeList
     }
 };
 </script>
