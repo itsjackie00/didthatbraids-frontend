7be2c5212248763c7a6c804c6fd26fccbadc21f5
itsjackie00 <jackiedanso33@gmail.com>
2025-01-13 12:45:24 +0100

    created braidtype list

---
+++ src/components/BraidstypeList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="braids container-fluid">
+        <div class="braidstype-list mt-3">
+            <h1>Tipi di treccine</h1>
+            <p>Scopri i nostri tipi di treccine!</p>
+        </div>
+        <BraidsCard />
+    </div>
+</template>
+
+<script>
+import BraidsCard from './cards/BraidsCard.vue';
+
+export default {
+    components: {
+        BraidsCard,
+    }
+};
+</script>
+
+<style scoped></style>
