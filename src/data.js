326eeeba26ce035d4bd6895e3072ea1d18b3f4d8
itsjackie00 <jackiedanso33@gmail.com>
2025-01-17 15:11:59 +0100

    added category in the array

--- src/data.js
+++ src/data.js
@@ -1,66 +1,76 @@
 const braidsData = [
-    {
-      type: "Box Braids",
-      description: "Trecce individuali divise in sezioni quadrate, note per la loro versatilità e come stile protettivo.",
-      image: "https://i.ytimg.com/vi/zDMYx63KdXw/maxresdefault.jpg",
-      price: 180, // Prezzo in euro
-      duration: 5 // Durata in ore
-    },
-    { 
-      type: "Senegalese Twists",
-      description: "Conosciute anche come rope twists, queste trecce utilizzano due ciocche attorcigliate con estensioni per un aspetto elegante.",
-      image: "https://i.pinimg.com/736x/c3/3f/ab/c33fab505531b46583561d78269bd59f.jpg",
-      price: 200,
-      duration: 6
-    },
-    {
-      type: "Knotless Braids",
-      description: "Una variante delle box braids che inizia con i capelli naturali, aggiungendo gradualmente le estensioni per ridurre la tensione sul cuoio capelluto.",
-      image: "https://polishedbraids.com/wp-content/uploads/2022/04/KNOTLESS-MEDUIMN-LAGRE-BRAIDS-LONG.jpg",
-      price: 170,
-      duration: 5
-    },
-    {
-      type: "Fulani Braids",
-      description: "Una combinazione di cornrows e trecce individuali, spesso adornate con perline, originarie del popolo Fula dell'Africa occidentale.",
-      image: "https://braidhairstyles.com/wp-content/uploads/Image-of-Fulani-Braids-With-Beads-inspired-by-Fulani-Braids-Hairstyles-941x1024.jpg",
-      price: 220,
-      duration: 4
-    },
-    {
-      type: "Goddess Braids",
-      description: "Trecce spesse e rialzate vicine al cuoio capelluto, che offrono un aspetto elegante e protettivo.",
-      image: "https://luemashairandbeauty.com/cdn/shop/files/medium-goddess-bohemian-knotless-braids.jpg?v=1702464552&width=1445",
-      price: 190,
-      duration: 3
-    },
-    {
-      type: "Cornrows",
-      description: "Trecce aderenti al cuoio capelluto, realizzate in linee dritte o in disegni intricati, utilizzate sia per motivi estetici che protettivi.",
-      image: "https://live-essnc.s3.amazonaws.com/uploads/2022/09/447812242_1124008972204855_7203842129939180764_n.jpg",
-      price: 120,
-      duration: 2
-    },
-    {
-      type: "Bohemian Box Braids",
-      description: "Una variante delle box braids che incorpora ciocche ondulate o ricce per un aspetto più boho e rilassato.",
-      image: "https://areneemua.com/cdn/shop/files/E5279266-82B8-40D7-A0D8-122304A0A6C3.jpg?v=1699505784",
-      price: 230,
-      duration: 6
-    },
-    {
-      type: "Soft Locs",
-      description: "Trecce aderenti al cuoio capelluto, realizzate in linee dritte o in disegni intricati, utilizzate sia per motivi estetici che protettivi.",
-      image: "https://i2.wp.com/www.hadviser.com/wp-content/uploads/2022/11/9-soft-locs-style-ChprtkRsgKj.jpg?resize=1002%2C1219&ssl=1",
-      price: 120,
-      duration: 2
-    },
-    {
-      type: "Crotchet Braids",
-      description: "Trecce aderenti al cuoio capelluto, realizzate in linee dritte o in disegni intricati, utilizzate sia per motivi estetici che protettivi.",
-      image: "https://m.media-amazon.com/images/I/81GfFcmKyPL.jpg",
-      price: 120,
-      duration: 4
-    }
-  ];
-export { braidsData };  
+  {
+    type: "Box Braids",
+    category: "braids", // Categoria aggiunta
+    description: "Trecce individuali divise in sezioni quadrate, note per la loro versatilità e come stile protettivo.",
+    image: "https://i.ytimg.com/vi/zDMYx63KdXw/maxresdefault.jpg",
+    price: 180, // Prezzo in euro
+    duration: 5 // Durata in ore
+  },
+  { 
+    type: "Senegalese Twists",
+    category: "twists", // Categoria aggiunta
+    description: "Conosciute anche come rope twists, queste trecce utilizzano due ciocche attorcigliate con estensioni per un aspetto elegante.",
+    image: "https://i.pinimg.com/736x/c3/3f/ab/c33fab505531b46583561d78269bd59f.jpg",
+    price: 200,
+    duration: 6
+  },
+  {
+    type: "Knotless Braids",
+    category: "braids", // Categoria aggiunta
+    description: "Una variante delle box braids che inizia con i capelli naturali, aggiungendo gradualmente le estensioni per ridurre la tensione sul cuoio capelluto.",
+    image: "https://polishedbraids.com/wp-content/uploads/2022/04/KNOTLESS-MEDUIMN-LAGRE-BRAIDS-LONG.jpg",
+    price: 170,
+    duration: 5
+  },
+  {
+    type: "Fulani Braids",
+    category: "braids", // Categoria aggiunta
+    description: "Una combinazione di cornrows e trecce individuali, spesso adornate con perline, originarie del popolo Fula dell'Africa occidentale.",
+    image: "https://braidhairstyles.com/wp-content/uploads/Image-of-Fulani-Braids-With-Beads-inspired-by-Fulani-Braids-Hairstyles-941x1024.jpg",
+    price: 220,
+    duration: 4
+  },
+  {
+    type: "Goddess Braids",
+    category: "braids", // Categoria aggiunta
+    description: "Trecce spesse e rialzate vicine al cuoio capelluto, che offrono un aspetto elegante e protettivo.",
+    image: "https://luemashairandbeauty.com/cdn/shop/files/medium-goddess-bohemian-knotless-braids.jpg?v=1702464552&width=1445",
+    price: 190,
+    duration: 3
+  },
+  {
+    type: "Cornrows",
+    category: "braids", // Categoria aggiunta
+    description: "Trecce aderenti al cuoio capelluto, realizzate in linee dritte o in disegni intricati, utilizzate sia per motivi estetici che protettivi.",
+    image: "https://live-essnc.s3.amazonaws.com/uploads/2022/09/447812242_1124008972204855_7203842129939180764_n.jpg",
+    price: 120,
+    duration: 2
+  },
+  {
+    type: "Bohemian Box Braids",
+    category: "braids", // Categoria aggiunta
+    description: "Una variante delle box braids che incorpora ciocche ondulate o ricce per un aspetto più boho e rilassato.",
+    image: "https://areneemua.com/cdn/shop/files/E5279266-82B8-40D7-A0D8-122304A0A6C3.jpg?v=1699505784",
+    price: 230,
+    duration: 6
+  },
+  {
+    type: "Soft Locs",
+    category: "locs", // Categoria aggiunta
+    description: "Una variante di dreadlocks morbidi e flessibili, che offrono un look naturale e moderno.",
+    image: "https://i2.wp.com/www.hadviser.com/wp-content/uploads/2022/11/9-soft-locs-style-ChprtkRsgKj.jpg?resize=1002%2C1219&ssl=1",
+    price: 120,
+    duration: 2
+  },
+  {
+    type: "Crotchet",
+    category: "crotchet", // Categoria aggiunta
+    description: "Un metodo di installazione rapido che utilizza un uncinetto per intrecciare le extension direttamente ai capelli naturali.",
+    image: "https://m.media-amazon.com/images/I/81GfFcmKyPL.jpg",
+    price: 120,
+    duration: 4
+  }
+];
+
+export { braidsData };
